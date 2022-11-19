1ecf1b5ba4d1306ff9d72438f55cf7d876e2c7cc
İbrahim Çetinkaya <ibrahim.ecetinkaya@gmail.com>
2022-11-19 19:33:02 +0000

    Template React Github

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
+import User from './User';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
+        <User />
         <p className="small">
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/User.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function User() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          GitHub Codespaces <span className="heart">♥️</span> React
+        </p> 
+        <p>
+          Ibrahim Cetinkaya
+        </p>  
+      </header>
+    </div>
+  );
+}
+
+export default User;
